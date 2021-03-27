4bf201521196a233f1bcbfc94e85e4442c1c9229
brdjaegh <bram.dejaegher@ugent.be>
2021-03-27 20:51:03 +0100

    change obis names

--- logger.py
+++ logger.py
@@ -25,33 +25,20 @@ debug = False
 
 # Add/update OBIS codes here:
 obiscodes = {
-    "0-0:1.0.0": "Timestamp",
-    "0-0:96.3.10": "Switch electricity",
-    "0-1:24.4.0": "Switch gas",
-    "0-0:96.1.1": "Meter serial electricity",
-    "0-1:96.1.1": "Meter serial gas",
-    "0-0:96.14.0": "Current rate (1=day,2=night)",
-    "1-0:1.8.1": "Rate 1 (day) - total consumption",
-    "1-0:1.8.2": "Rate 2 (night) - total consumption",
-    "1-0:2.8.1": "Rate 1 (day) - total production",
-    "1-0:2.8.2": "Rate 2 (night) - total production",
-    "1-0:21.7.0": "L1 consumption",
-    "1-0:41.7.0": "L2 consumption",
-    "1-0:61.7.0": "L3 consumption",
-    "1-0:1.7.0": "All phases consumption",
-    "1-0:22.7.0": "L1 production",
-    "1-0:42.7.0": "L2 production",
-    "1-0:62.7.0": "L3 production",
-    "1-0:2.7.0": "All phases production",
-    "1-0:32.7.0": "L1 voltage",
-    "1-0:52.7.0": "L2 voltage",
-    "1-0:72.7.0": "L3 voltage",
-    "1-0:31.7.0": "L1 current",
-    "1-0:51.7.0": "L2 current",
-    "1-0:71.7.0": "L3 current",
-    "0-1:24.2.3": "Gas consumption"
+    "0-0:1.0.0": "TIMESTAMP",
+    "0-0:96.3.10": "SWITCH_ELECTRICITY",
+    "0-1:24.4.0": "SWITCH_GAS",
+    "0-0:96.14.0": "DAY_OR_NIGHT",
+    "1-0:1.8.1": "CONSUME_DAY_INT",
+    "1-0:1.8.2": "CONSUME_NIGHT_INT",
+    "1-0:2.8.1": "PRODUCE_DAY_INT",
+    "1-0:2.8.2": "PRODUCE_NIGHT_INT",
+    "1-0:1.7.0": "CONSUME",
+    "1-0:2.7.0": "PRODUCE",
+    "1-0:32.7.0": "VOLTAGE",
+    "1-0:31.7.0": "CURRENT",
+    "0-1:24.2.3": "GAS_CONSUME_INT"
     }
-
 
 def checkcrc(p1telegram):
     # check CRC16 checksum of telegram and return False if not matching
