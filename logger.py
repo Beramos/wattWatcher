545804ebb44c1e6b16d0b91713f8256635786342
beramos <bram.de.jaegher@gmail.com>
2021-03-27 09:56:53 +0100

    add log script

---
+++ logger.py
@@ -0,0 +1,10 @@
+'''
+    File name: logger.py
+    Author: Bram De Jaegher
+    Date created: 24/03/2021
+    Date last modified: 24/03/2021
+    Python Version: 3.6
+'''
+
+import sqlite3
+conn = sqlite3.connect("energy.db")
